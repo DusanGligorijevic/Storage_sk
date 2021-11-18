440e779329bec26bd6b7055b2388e875b132182a
DusanGligorijevic <dukacode@gmail.com>
2021-11-18 23:43:26 +0100

    asd

--- Storage/src/storage/Storage.java
+++ Storage/src/storage/Storage.java
@@ -104,7 +104,7 @@ public abstract class Storage{
      */
     public abstract void previewDir(String path);
     
-    public abstract void preview();
+  
     
     /**
      * Ova metoda se koristi za skidanje
